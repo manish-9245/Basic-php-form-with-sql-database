de1f87f9c891d0305f9ce2cb4a2ff98099df5901
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2021-06-14 12:48:40 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,10 +3,11 @@ $insert=false;
 if(isset($_POST['name'])){
     //Set connection variables
     $server="localhost";
-    $username="root";
-    $password="";
+    $username="manishti_trip";
+    $password="Password@9245";
+    $dbname="manishti_trip";
     // Create a connection
-    $con=mysqli_connect($server,$username,$password);
+    $con=mysqli_connect($server,$username,$password,$dbname);
     // Check for connection success
     if (!$con) {
         # code...
@@ -21,7 +22,7 @@ if(isset($_POST['name'])){
     $email= $_POST['email'];
     $phone= $_POST['phone'];
     $desc= $_POST['desc'];
-    $sql="INSERT INTO `trip` . `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    $sql="INSERT INTO `manishti_trip` . `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
     // echo $sql;
     // Execute the query
     if($con->query($sql)==true){
